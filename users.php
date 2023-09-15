623ad98ffc8bd039606c79aa87d6493201faf73f
usmanalameer <muhammadusmanalameer@gmail.com>
2023-09-16 01:20:54 +0500

    first

---
+++ users.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+   
+
+
+
+<div class="container mt-5 ">
+
+  <form method="post" >
+    <div  >
+      <label >Name</label>
+      <input autocomplete="none" type="name"  placeholder="Enter your name" name="name" > 
+    </div>
+    <div  >
+      <label >Email</label>
+      <input type="email"  placeholder="Enter your email" name="eamil" > 
+    </div>
+    <div  >
+      <label >mobile</label>
+      <input type="mobile"  placeholder="Enter your mobile number" name="mobile" > 
+    </div>
+    <div  >
+      <label >Password</label>
+      <input type="passwoed"  placeholder="Enter your Password" name="password" > 
+    </div>
+    <div  >
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+
+
+
+
+
+<div class="container">
+
+<form method="post" >
+
+  <div class="mb-3">
+    <label  class="form-label">Name</label>
+    <input type="text" class="form-control"  name="name" >
+  </div>
+  
+  <div class="mb-3">
+    <label  class="form-label">Email</label>
+    <input type="email" class="form-control" name="email"   autocomplete="off" >
+</div>
+
+  
+  <div class="mb-3">
+    <label  class="form-label">Number</label>
+    <input type="number" class="form-control" name="number" autocomplete="off" > 
+  </div>
+  
+  <div class="mb-3">
+    <label  class="form-label">Password</label>
+    <input type="password" class="form-control" name="password" autocomplete="off" >
+  </div>
+  
+  
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
+</div>
+    
+  </body>
+</html>
